a4dc52205b19d3f1bb5238431d055c01808a3f6a
Jeremy <jeremy.morel.62@gmail.com>
2026-06-15 16:11:04 +0200

    Ajout du portfolio de Jeremy

---
+++ script.js
@@ -0,0 +1,9 @@
+let nbLikes = 0;
+const boutonLike = document.getElementById("mon-bouton-like");
+
+if (boutonLike) {
+boutonLike.addEventListener("click", () => {
+nbLikes = nbLikes + 1;
+boutonLike.textContent = "❤️ J'aime (" + nbLikes + ")";
+});
+}
